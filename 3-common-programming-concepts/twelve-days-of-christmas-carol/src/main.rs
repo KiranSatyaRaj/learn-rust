3e0913ffe30ecc5ce04607d9584d482b762c931f
KiranSatyaRaj <kiranjamy20021508@gmail.com>
2025-02-22 19:45:38 +0530

    add twelve-day-of-christmas-carol exercise

---
+++ 3-common-programming-concepts/twelve-days-of-christmas-carol/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    let numbers = [
+        "first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth"
+    ];
+
+    let lyrics = [
+      "and a partridge in a pear tree.",
+        "Two turtle dove,",
+        "Three French hens,",
+        "Four calling birds,",
+        "Five golden rings,",
+        "Six geese-a-laying,",
+        "Seven swans-a-swimming,",
+        "Eight maids-a-milking,",
+        "Nine ladies a dancing,",
+        "Ten lords a-leaping,",
+        "Eleven pipers piping,",
+        "Twelve drummers drumming,",
+    ];
+
+    for number in 0..12 {
+        println!("On the {} day of Christmas,", numbers[number]);
+        println!("my true love gave to me");
+        if number == 0 {
+            println!("A partridge in a pear tree.");
+        } else {
+            for lyric in (0..number + 1).rev() {
+                println!("{}", lyrics[lyric]);
+            }
+        }
+        println!();
+    }
+}
